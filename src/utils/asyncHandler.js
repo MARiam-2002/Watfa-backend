c58a79808f98b6fc64ca5fdd8ce9c2d561f79bf4
mariam khaled <khaldmrym769@gmail.com>
2024-12-03 22:14:38 +0200

    update Auth Api and add countriesWithFlagsAndPhoneCodes

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,13 @@
 export const asyncHandler = (fn) => {
   return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch((error) => {
+      // إذا لم يتم إرسال استجابة بعد، مرر الخطأ إلى Middleware الأخطاء
       if (!res.headersSent) {
-        return next(
+        next(
           new Error(error.message || "Internal Server Error", { cause: 500 })
         );
+      } else {
+        console.error("Error response has already been sent.");
       }
     });
   };
@@ -12,12 +15,14 @@ export const asyncHandler = (fn) => {
 
 export const globalErrorHandling = (error, req, res, next) => {
   if (!res.headersSent) {
-    return res.status(error.cause || 400).json({
-      msgError: error.message,
-      stack: error.stack,
+    const statusCode = error.cause || 500; // الحالة الافتراضية 500 إذا لم يتم تحديد سبب
+    return res.status(statusCode).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+      stack: process.env.NODE_ENV === "development" ? error.stack : undefined, // إظهار الـ stack فقط في وضع التطوير
     });
   } else {
     console.error("Error response has already been sent.");
-    return next(error);
+    next(error); // تمرير الخطأ في حال تم إرسال الاستجابة مسبقًا
   }
 };
