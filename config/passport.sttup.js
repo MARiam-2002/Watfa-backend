e6e6ccf4470baae48cb2ef04d2e50023f2a1ad03
mariam khaled <khaldmrym769@gmail.com>
2024-12-04 01:26:24 +0200

    update Auth Api and add countriesWithFlagsAndPhoneCodes

--- config/passport.sttup.js
+++ config/passport.sttup.js
@@ -6,7 +6,30 @@ import dotenv from "dotenv";
 import axios from "axios"; // نحتاج إلى axios لإرسال طلب إلى Google People API
 dotenv.config();
 
+import { google } from "googleapis";
 
+async function getPhoneNumber(accessToken) {
+  try {
+    const peopleService = google.people({ version: 'v1', auth: accessToken });
+    
+    // قم بتحميل بيانات المستخدم من People API
+    const res = await peopleService.people.get({
+      resourceName: 'people/me',
+      personFields: 'phoneNumbers',
+    });
+
+    // استخراج رقم الهاتف إذا كان موجودًا
+    const phoneNumbers = res.data.phoneNumbers;
+    if (phoneNumbers && phoneNumbers.length > 0) {
+      return phoneNumbers[0].value; // يرجع أول رقم هاتف موجود
+    } else {
+      return null; // لا يوجد رقم هاتف
+    }
+  } catch (error) {
+    console.error("Error fetching phone number: ", error);
+    return null;
+  }
+}
 
 passport.use(
   new GoogleStrategy(
@@ -14,13 +37,13 @@ passport.use(
       clientID: process.env.CLIENTID,
       clientSecret: process.env.CLIENTSECRET,
       callbackURL: "https://watfa-backend.vercel.app/auth/google/callback",
-      passReqToCallback: true, // يتيح تمرير req إلى الوظيفة
+      passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, done) => {
       try {
         const role = req.session.role || "buyer";
 
-        // جلب رقم الهاتف
+        // جلب رقم الهاتف باستخدام getPhoneNumber
         const phoneNumber = await getPhoneNumber(accessToken);
 
         let user = await userModel.findOne({
@@ -48,7 +71,7 @@ passport.use(
             id: user._id,
             email: user.email,
             userName: user.userName,
-            phoneNumber: user.phoneNumber, // تضمين رقم الهاتف في الـ token
+            phoneNumber: user.phoneNumber,
             role: user.role,
           },
           process.env.TOKEN_KEY,
@@ -62,6 +85,7 @@ passport.use(
     }
   )
 );
+
 
 
 passport.serializeUser((user, done) => {
