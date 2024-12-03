00132cdf510c06bd5bcc130038b4c6897630c978
mariam khaled <khaldmrym769@gmail.com>
2024-12-03 22:19:26 +0200

    update Auth Api and add countriesWithFlagsAndPhoneCodes

--- src/middleware/validation.middleware.js
+++ src/middleware/validation.middleware.js
@@ -1,11 +1,5 @@
 import { Types } from "mongoose";
 
-export const isValidObjectId = (value, helper) => {
-  return Types.ObjectId.isValid(value)
-    ? true
-    : helper.message("Invalid ObjectId");
-};
-
 export const isValidation = (Schema) => {
   return (req, res, next) => {
     const copyReq = {
@@ -14,13 +8,23 @@ export const isValidation = (Schema) => {
       ...req.query,
       ...req.files,
     };
+
     const validationResult = Schema.validate(copyReq, { abortEarly: false });
+
     if (validationResult.error) {
       const errorMessages = validationResult.error.details.map(
         (error) => error.message
       );
-      return next(new Error(errorMessages), { cause: 400 });
+
+      // إعادة الخطأ بشكل استجابة JSON مع الحالة 400
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: errorMessages,
+      });
     }
+
+    // إذا لم يكن هناك خطأ، متابعة الطلب
     return next();
   };
 };
